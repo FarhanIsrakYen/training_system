629cf0ab1ab2f042c169f8e87889a60f4237707e
FarhanIsrakYen <farhan.yen@northsouth.edu>
2024-11-05 01:46:00 +0600

    updated activity controller for request

--- app/controllers/ActivityController.js
+++ app/controllers/ActivityController.js
@@ -3,6 +3,11 @@ import { validateRegisterActivity } from "../requests/activityRequests.js";
 import { CreateActivity, DeleteActivity, GetActivities, UpdateActivity } from "../services/ActivityServices.js";
 
 export const createActivity = async(req, res) => {
+    await Promise.all(validateRegisterActivity.map(validation => validation.run(req)));
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ message: "data cannot be processed", errors: errors.array() });
+    }
     let result = await CreateActivity(req)
     return res.status(result.statusCode).json({
         message: result.message
@@ -24,12 +29,6 @@ export const deleteActivity = async(req, res) => {
 }
 
 export const getActivities = async(req, res) => {
-    await Promise.all(validateRegisterActivity.map(validation => validation.run(req)));
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ message: "data cannot be processed", errors: errors.array() });
-    }
-
     let result = await GetActivities(req)
     if (result.statusCode === 200) {
         return res.status(result.statusCode).json({
